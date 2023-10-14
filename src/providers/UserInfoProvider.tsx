a63d4d001ab68aed6ffd2a94b78d65d78b5c0626
bngnly <bnguonly19@gmail.com>
2023-10-14 17:36:22 -0400

    Able to reload the user data after the updates

--- src/providers/UserInfoProvider.tsx
+++ src/providers/UserInfoProvider.tsx
@@ -1,8 +1,10 @@
 import {createContext, ReactNode, useEffect, useState} from 'react';
+import { getUser } from '../services/userService';
 import {getUser_storage} from '../storage/UserStorage';
 import {LocationData, User} from '../types/types';
 
 export type UserInfoContextType = {
+  reloadData: () => void;
   user: User;
   setUser: React.Dispatch<React.SetStateAction<User>>;
   locationData: LocationData;
@@ -21,6 +23,7 @@ const defaultLocation: LocationData = {
 };
 
 export const UserInfoContext = createContext<UserInfoContextType>({
+  reloadData: () => {},
   user: guestUser,
   setUser: () => {},
   locationData: defaultLocation,
@@ -28,22 +31,28 @@ export const UserInfoContext = createContext<UserInfoContextType>({
 });
 
 function UserInfoProvider({children}: {children: ReactNode}) {
+  const [reload, setReload] = useState(false);
   const [user_state, setUser_state] = useState<User>(guestUser);
   const [locationData_state, setLocationData_state] =
     useState<LocationData>(defaultLocation);
 
+  const reloadData = () => {
+    setReload(!reload);
+  };
+
   const getUser_provider = async () => {
-    const user = await getUser_storage();
+    const user = await getUser(user_state.id)
     setUser_state(user);
   };
 
   useEffect(() => {
     getUser_provider();
-  }, []);
+  }, [reload]);
 
   return (
     <UserInfoContext.Provider
       value={{
+        reloadData,
         user: user_state,
         setUser: setUser_state,
         locationData: locationData_state,
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -9,14 +9,14 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {Image, Text} from 'react-native';
 import ActionButton from '../components/common/ActionButton';
 import BottomNavBar from '../components/homeScreen/BottomNavBar';
-import {useContext, useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {UserInfoContext} from '../providers/UserInfoProvider';
 import SingleLineInput from '../components/common/SingleLineTextInput';
 import {updateUserInfo} from '../services/userService';
 
 const ProfileScreen = () => {
   const {fontScale} = useWindowDimensions();
-  const {user} = useContext(UserInfoContext);
+  const {reloadData, user} = useContext(UserInfoContext);
   const [editMode, setEditMode] = useState(false);
   const [username, setUserName] = useState(user.username);
   const [firstName, setFirstName] = useState(user.firstName);
@@ -133,6 +133,7 @@ const ProfileScreen = () => {
               onPress={async () => {
                 setEditMode(false);
                 await updateUserInfo(user.id, {firstName, lastName, username});
+                reloadData();
               }}
               text={'Save Profile'}
               primary={true}
